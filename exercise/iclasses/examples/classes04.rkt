bbe54a8c023e9915ef7a26accad7d9a52c8cc691
Leonardo Reis <leonardo.vsreis@gmail.com>
2023-07-04 18:12:43 -0300

    modifying example 04 of iclassess

--- exercise/iclasses/examples/classes04.rkt
+++ exercise/iclasses/examples/classes04.rkt
@@ -1,26 +1,26 @@
-#lang dcc019/classes
+#lang dcc019/exercise/iclasses
 
 class c1 extends object
   field x
   field y
-  method initialize() 1
-  method setx1(v) set x = v
-  method sety1(v) set y = v
-  method getx1() x
-  method gety1() y
+  method initialize() return 1
+  method setx1(v) x = v
+  method sety1(v) y = v
+  method getx1() return x
+  method gety1() return y
 
 class c2 extends c1
   field y
-  method sety2(v) set y = v
-  method getx2() x
-  method gety2() y
+  method sety2(v) y = v
+  method getx2() return x
+  method gety2() return y
 
-let o2 = new c2()
-in begin
+var o2; {
+  o2 = new c2();
   send o2 setx1(101);
   send o2 sety1(102);
   send o2 sety2(999);
-  -(send o2 gety1(), send o2 gety2())
-end
+  print -(send o2 gety1(), send o2 gety2())
+}
 
 % Result: -897
